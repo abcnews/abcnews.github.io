0b8175504ae63a993701c1d9517abe9ffefb1b00
Joshua Byrd <phocks@gmail.com>
2023-02-10 12:04:14 +1000

    Try the head title again

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,30 +1,25 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
-import { useRouter } from 'next/router'
-import { useConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
+import { NextSeo } from 'next-seo';
 
 const config: DocsThemeConfig = {
-  head: () => {
-    const { asPath } = useRouter()
-    const { frontMatter } = useConfig()
-    return <>
-      <title>{frontMatter.title + '- ABC Dev'}</title>
-      <meta property="og:url" content={`https://abcnews.github.io${asPath}`} />
-      <meta property="og:title" content={frontMatter.title || 'ABC Dev'} />
-      <meta property="og:description" content={frontMatter.description || 'News development site.'} />
-    </>
+  useNextSeoProps() {
+    return {
+      titleTemplate: "%s - ABC Dev",
+    };
   },
   logo: <span>ABC Dev</span>,
   project: {
-    link: 'https://github.com/abcnews',
+    link: "https://github.com/abcnews",
   },
   // chat: {
   //   link: 'https://discord.com',
   // },
-  docsRepositoryBase: 'https://github.com/abcnews/abcnews.github.io/tree/nextra',
+  docsRepositoryBase:
+    "https://github.com/abcnews/abcnews.github.io/tree/nextra",
   footer: {
-    text: 'ABC News',
+    text: "ABC News",
   },
-}
+};
 
-export default config
+export default config;
