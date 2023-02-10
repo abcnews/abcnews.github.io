479bfca1522d1a40a45aa4c52561b1873f90f77b
Joshua Byrd <phocks@gmail.com>
2023-02-10 11:47:35 +1000

    Head?

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,7 +1,18 @@
 import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
+import { useRouter } from 'next/router'
+import { useConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
+  head: () => {
+    const { asPath } = useRouter()
+    const { frontMatter } = useConfig()
+    return <>
+      <meta property="og:url" content={`https://my-app.com${asPath}`} />
+      <meta property="og:title" content={frontMatter.title || 'ABC Dev'} />
+      <meta property="og:description" content={frontMatter.description || 'News development site.'} />
+    </>
+  },
   logo: <span>ABC Dev</span>,
   project: {
     link: 'https://github.com/abcnews',
