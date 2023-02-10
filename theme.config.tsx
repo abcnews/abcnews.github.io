8601c2d5cb459b7ac2ec3fb1a224ce230ce1945f
Joshua Byrd <phocks@gmail.com>
2023-02-10 11:15:22 +1000

    Maybe try a github action next

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>ABC Dev</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/abcnews',
   },
   chat: {
     link: 'https://discord.com',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/abcnews/abcnews.github.io/tree/main',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'ABC News',
   },
 }
 
